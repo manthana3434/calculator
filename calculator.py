8ba4b96f7ff7428069400b0e773082743bbabf67
manthana <manthanahc@gmail.com>
2026-09-18 16:28:51 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
  a = float(input("Enter first number:"))
  b = float(input("Enter second number:"))
 
-   print("Addition=",a + b)
-   print("Substraction=",a - b)
-   print("Multiplication=",a * b)
-   print("Division=",a / b) 
+   print("Addition=", a + b)
+   print("Substraction=", a - b)
+   print("Multiplication=", a * b)
+   print("Division=", a / b) 
