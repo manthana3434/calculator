3f7707d056fc9d566a25981b7c7cb8a1bf64bc71
manthana <manthanahc@gmail.com>
2026-09-18 15:32:21 +0530

    Implement basic calculator for addition and subtraction
    Fix typo in 'float' and correct variable name for subtraction.

---
+++ calculator.py
@@ -0,0 +1,7 @@
+ a = float(input("Enter first number:"))
+ b = flaot(input("Enter second number:"))
+
+addition=a+b
+substraction=a-b
+print("Addition=",addition)
+print("substraction=",substraction)
